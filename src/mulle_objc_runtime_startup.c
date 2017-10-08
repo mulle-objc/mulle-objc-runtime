5d80d73506a8c7db7e9a0613b079beeea6ea4982
Nat! <nat@mulle-kybernetik.com>
2017-10-08 18:39:56 +0200

    add missing file

---
+++ src/mulle_objc_runtime_startup.c
@@ -0,0 +1,52 @@
+//
+//  mulle_standalone_objc_universe.c
+//  mulle-objc-runtime
+//
+//  Created by Nat! on 21.01.16.
+//  Copyright (c) 2016 Nat! - Mulle kybernetiK.
+//  Copyright (c) 2016 Codeon GmbH.
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//  Redistributions of source code must retain the above copyright notice, this
+//  list of conditions and the following disclaimer.
+//
+//  Redistributions in binary form must reproduce the above copyright notice,
+//  this list of conditions and the following disclaimer in the documentation
+//  and/or other materials provided with the distribution.
+//
+//  Neither the name of Mulle kybernetiK nor the names of its contributors
+//  may be used to endorse or promote products derived from this software
+//  without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+//  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+//  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+//  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+//  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+//  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
+//
+#include "mulle_objc_runtime.h"
+
+#include <mulle_test_allocator/mulle_test_allocator.h>
+
+
+MULLE_C_CONST_RETURN  // always returns same value (in same thread)
+struct _mulle_objc_universe  *__get_or_create_mulle_objc_universe( void)
+{
+   struct _mulle_objc_universe   *universe;
+
+   universe = __mulle_objc_get_universe();
+   if( _mulle_objc_universe_is_uninitialized( universe))
+      _mulle_objc_universe_bang( universe, 0, 0, NULL);
+   
+   return( universe);
+}
+
