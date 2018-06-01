4242a17891a916fe09f12ae4124a7fb5c5d8702e
Nat! <nat@mulle-kybernetik.com>
2018-06-01 19:13:25 +0200

    no more brew formula

--- mulle-project/formula-info.sh
+++
@@ -1,9 +0,0 @@
-PROJECT="mulle-objc-runtime"
-DESC="An Objective-C runtime, written 100% in C"
-DEPENDENCIES='${DEPENDENCY_TAP}mulle-concurrent
-${DEPENDENCY_TAP}mulle-vararg'
-
-
-BUILD_DEPENDENCIES='${BOOTSTRAP_TAP}mulle-build
-${BOOTSTRAP_TAP}mulle-bootstrap
-cmake'
