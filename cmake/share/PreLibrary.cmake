ab34d924fd0500508fc54f8ae780b286aea0039d
Nat! <nat@mulle-kybernetik.com>
2021-11-12 16:48:23 +0100

    push unwanted warnings cmake fix to projects

--- cmake/share/PreLibrary.cmake
+++ cmake/share/PreLibrary.cmake
@@ -8,8 +8,8 @@ if( MULLE_TRACE_INCLUDE)
    message( STATUS "# Include \"${CMAKE_CURRENT_LIST_FILE}\"" )
 endif()
 
-include( UnwantedWarningsC)
 include( CompilerDetectionC)
+include( UnwantedWarningsC) # after detection, before flags
 include( CompilerFlagsC)
 
 include( PreLibraryAuxC OPTIONAL)
